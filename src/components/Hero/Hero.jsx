7ff752344c266751ad5ee847b3691a96cbd69226
Danico-Mfala <mfaladan@gmail.com>
2023-04-25 08:40:33 -0700

    hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hero = () => {
+  <section className="hero-wrapper">
+    <div className="paddings innerWidth flexCenter hero-container">
+      <div className="hero-left">Left section</div>
+      <div className="flexCenter hero-right">
+        <div className="image-container">
+          <img src="./hero-image.png" alt="" />
+        </div>
+      </div>
+    </div>
+  </section>;
+};
+
+export default Hero;
