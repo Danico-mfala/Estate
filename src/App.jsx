9feecaeef4238daa1f22ce16de1d9977a3c70107
Danico-Mfala <mfaladan@gmail.com>
2023-04-25 10:05:23 -0700

    hero search part

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/header";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,42 @@
 import React from "react";
+import "./hero.css";
+import { HiLocationMarker } from "react-icons/hi";
 
 const Hero = () => {
-  <section className="hero-wrapper">
-    <div className="paddings innerWidth flexCenter hero-container">
-      <div className="hero-left">Left section</div>
-      <div className="flexCenter hero-right">
-        <div className="image-container">
-          <img src="./hero-image.png" alt="" />
+  return (
+    <section className="hero-wrapper">
+      <div className="paddings innerWidth flexCenter hero-container">
+        <div className="hero-left">
+          {/* Left side */}
+          <div className="flexColStart hero-left">
+            <div className="hero-title">
+              <div className="orange-circle" />
+              <h1>
+                Discover <br /> Most Suitable
+                <br /> Property
+              </h1>
+            </div>
+            <div className="flexColStart hero-des">
+              <span>
+                Find a variety of Properties that suit you very easilty
+              </span>
+              <span>
+                Forget all difficulties in finding a residence for you
+              </span>
+            </div>
+            <div className="search-bar">
+              <HiLocationMarker color="var(--blue)" size={23} />
+            </div>
+          </div>
+        </div>
+        <div className="flexCenter hero-right">
+          <div className="image-container">
+            <img src="./hero-image.png" alt="" />
+          </div>
         </div>
       </div>
-    </div>
-  </section>;
+    </section>
+  );
 };
 
 export default Hero;
