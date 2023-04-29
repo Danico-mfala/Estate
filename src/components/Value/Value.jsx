3046037e51f9daec671d456fd4999f0e1aaba30f
Danico-Mfala <mfaladan@gmail.com>
2023-04-29 12:55:44 -0700

    contact section

--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Accordion,
   AccordionItem,
@@ -38,14 +38,34 @@ const Value = () => {
             preExpanded={[0]}
           >
             {data.map((item, i) => {
+              const [className, setClassName] = useState(null);
               return (
-                <AccordionItem className="accordionItem" key={i} uii={i}>
+                <AccordionItem
+                  className={`accordionItem ${className}`}
+                  key={i}
+                  uiid={i}
+                >
                   <AccordionItemHeading>
-                    <AccordionItemButton>
+                    <AccordionItemButton className="flexCenter accordionButton">
+                      <AccordionItemState>
+                        {({ expanded }) =>
+                          expanded
+                            ? setClassName("expanded")
+                            : setClassName("collapsed")
+                        }
+                      </AccordionItemState>
+
                       <div className="flexCenter icon">{item.icon}</div>
                       <span className="primaryText">{item.heading}</span>
+                      <div className="flexCenter icon">
+                        <MdOutlineArrowDropDown size={20} />
+                      </div>
                     </AccordionItemButton>
                   </AccordionItemHeading>
+
+                  <AccordionItemPanel>
+                    <p className="secondaryText">{item.detail}</p>
+                  </AccordionItemPanel>
                 </AccordionItem>
               );
             })}
