f12bbb31af2b158d0608d433a93fb67a65b1f8b5
Danico-Mfala <mfaladan@hotmail.com>
2023-05-03 23:32:38 +0300

    contact fix and done

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -19,7 +19,7 @@ const Contact = () => {
 
           <div className="flexColStart contactModes">
             {/* first row */}
-            <div className="flexColStart row">
+            <div className="flexStart row gap_1">
               <div className="flexColCenter mode">
                 <div className="flexStart">
                   <div className="flexCenter icon">
@@ -32,19 +32,49 @@ const Contact = () => {
                 </div>
                 <div className="flexCenter button">Call Now</div>
               </div>
+
+              {/* second mode */}
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <BsFillChatDotsFill size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Chat</span>
+                    <span className="secondaryText">021 123 145 14</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">Chat Now</div>
+              </div>
             </div>
             {/* second row */}
-            <div className="flexColCenter mode">
-              <div className="flexStart">
-                <div className="flexCenter icon">
-                  <MdCall size={25} />
+            <div className="flexStart row gap_1">
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <BsFillChatDotsFill size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Video Call</span>
+                    <span className="secondaryText">021 123 145 14</span>
+                  </div>
                 </div>
-                <div className="flexColStart detail">
-                  <span className="primaryText">Call</span>
-                  <span className="secondaryText">021 123 145 14</span>
-                </div>
+                <div className="flexCenter button">Call Now</div>
               </div>
-              <div className="flexCenter button">Call Now</div>
+
+              {/* fourth mode */}
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <HiChatBubbleBottomCenter size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Message</span>
+                    <span className="secondaryText">021 123 145 14</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">Message Now</div>
+              </div>
             </div>
           </div>
         </div>
